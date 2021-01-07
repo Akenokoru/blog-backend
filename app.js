0a91908a35730e7d4c40662ac990b11c63641929
panyuhang <panyuhang@xiaoyezi.com>
2021-01-07 16:25:21 +0800

    Init api router && handlers.

--- app.js
+++ app.js
@@ -1,56 +1,31 @@
-// const http = require('http');
-// const querystring = require('querystring');
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
 
-// const server = http.createServer((req, res) => {
-//   const method = req.method
-//   const url = req.url
-//   const path = url.split('?')[0]
-//   const query = querystring.parse(url.split('?')[1])
-  
-//   // set response header
-//   res.setHeader('Content-type', 'application/json')
-
-//   // set response data
-//   const resData = {
-//     method,
-//     url,
-//     path,
-//     query
-//   }
-  
-//   // GET method handler
-//   if (method === 'GET') {
-//     res.end(
-//       JSON.stringify(resData)
-//     )
-//   }
-
-//   // POST method handler
-//   if (method === 'POST') {
-//     let postData = ''
-//     req.on('data', chunk => {
-//       postData += chunk.toString()
-//     })
-//     req.on('end', () => {
-//       resData.postData = postData
-//       res.end(
-//         JSON.stringify(resData)
-//       )
-//     })
-//   }
-// })
 const serverHandler = (req, res) => {
   res.setHeader('Content-type', 'application/json')
 
-  const resData = {
-    name: 'Andrew',
-    site: 'xyz',
-    env: process.env.NODE_ENV
+  // blog router handler
+  const blogData = handleBlogRouter(req, res)
+  if (blogData) {
+    res.end(
+      JSON.stringify(blogData)
+    )
+    return
   }
 
-  res.send(
-    JSON.stringify(resData)
-  )
+  // user router handler
+  const userData = handleUserRouter(req, res)
+  if (userData) {
+    res.end(
+      JSON.stringify(userData)
+    )
+    return
+  }
+
+  // return 404
+  res.writeHead(404, {"Content-type": "text/plain"})
+  res.write("404 not found")
+  res.end()
 }
 
 module.exports = serverHandler
