fee622152ba04b80bdfcfc9bee6d94194250265e
panyuhang <panyuhang@xiaoyezi.com>
2021-01-07 15:02:14 +0800

    Change app.js.

--- app.js
+++ app.js
@@ -1,44 +1,56 @@
-const http = require('http');
-const querystring = require('querystring');
+// const http = require('http');
+// const querystring = require('querystring');
 
-const server = http.createServer((req, res) => {
-  const method = req.method
-  const url = req.url
-  const path = url.split('?')[0]
-  const query = querystring.parse(url.split('?')[1])
+// const server = http.createServer((req, res) => {
+//   const method = req.method
+//   const url = req.url
+//   const path = url.split('?')[0]
+//   const query = querystring.parse(url.split('?')[1])
   
-  // set response header
+//   // set response header
+//   res.setHeader('Content-type', 'application/json')
+
+//   // set response data
+//   const resData = {
+//     method,
+//     url,
+//     path,
+//     query
+//   }
+  
+//   // GET method handler
+//   if (method === 'GET') {
+//     res.end(
+//       JSON.stringify(resData)
+//     )
+//   }
+
+//   // POST method handler
+//   if (method === 'POST') {
+//     let postData = ''
+//     req.on('data', chunk => {
+//       postData += chunk.toString()
+//     })
+//     req.on('end', () => {
+//       resData.postData = postData
+//       res.end(
+//         JSON.stringify(resData)
+//       )
+//     })
+//   }
+// })
+const serverHandler = (req, res) => {
   res.setHeader('Content-type', 'application/json')
 
-  // set response data
   const resData = {
-    method,
-    url,
-    path,
-    query
-  }
-  
-  // GET method handler
-  if (method === 'GET') {
-    res.end(
-      JSON.stringify(resData)
-    )
+    name: 'Andrew',
+    site: 'xyz',
+    env: process.env.NODE_ENV
   }
 
-  // POST method handler
-  if (method === 'POST') {
-    let postData = ''
-    req.on('data', chunk => {
-      postData += chunk.toString()
-    })
-    req.on('end', () => {
-      resData.postData = postData
-      res.end(
-        JSON.stringify(resData)
-      )
-    })
-  }
-})
+  res.send(
+    JSON.stringify(resData)
+  )
+}
 
-server.listen(3000);
-console.log('Listening on port 3000');
+module.exports = serverHandler
